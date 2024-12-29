086b57562b9e46139260f59a5c6f9d7c28f11047
Erick Carrasco Oliveira <104008021+kcarrasc0@users.noreply.github.com>
2024-12-29 03:38:46 +0000

    gwsgw

--- index.js
+++ index.js
@@ -5,29 +5,16 @@ document.querySelector('.contact-button-container .btn').addEventListener('click
     console.log('Formulário de contato aberto');
   });
 
-  document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("form");
-    const popup = document.getElementById("popup");
-    const closePopup = document.getElementById("close-popup");
+const button = document.querySelector('form button')
+const modal = document.querySelector("dialog")
+const buttonClose = document.querySelector("dialogbutton")
+
+button.onclick = function () {
+  Modal.showModal()
+
+}
   
-    form.addEventListener("submit", (e) => {
-      e.preventDefault(); // Impede o envio padrão do formulário
-      // Lógica para enviar o email (ex.: via API ou backend)
-      // Simulação de envio:
-      setTimeout(() => {
-        popup.style.display = "flex"; // Exibe o modal
-      }, 500); // Atraso simulado
-    });
-  
-    closePopup.addEventListener("click", () => {
-      popup.style.display = "none"; // Fecha o modal ao clicar no "x"
-    });
-  
-    // Fechar o modal ao clicar fora do conteúdo
-    window.addEventListener("click", (e) => {
-      if (e.target === popup) {
-        popup.style.display = "none";
-      }
-    });
-  });
-  
+
+buttonClose.onclick = function () {
+  modal.close()
+}
