a7fd54a83acd6b4beea13aa818f0858a89eb9dce
Erick Carrasco Oliveira <104008021+kcarrasc0@users.noreply.github.com>
2024-12-29 03:56:56 +0000

    ww

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Se você precisar de algum JavaScript adicional para o botão, adicione no arquivo scripts/index.js
+
+// Example: Log a message when the contact button is clicked
+document.querySelector('.contact-button-container .btn').addEventListener('click', function() {
+    console.log('Formulário de contato aberto');
+  });
+
+const button = document.querySelector('form button')
+const modal = document.querySelector("dialog")
+const buttonClose = document.querySelector("dialogbutton")
+
+button.onclick = function () {
+  Modal.showModal()
+
+}
+  
+
+buttonClose.onclick = function () {
+  modal.close()
+}
